9fc223f2af7f4628ed7798d8e2ea323f4ac2a4e8
Igor <igor132143@gmail.com>
2023-07-26 15:24:28 +0300

    added JS logic

--- app.js
+++ app.js
@@ -33,19 +33,52 @@ const quizData = [
     },
 ];
 
-const questionE1 = document.getElementById('question');
+const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
 
-let currentQuestion = 0;
+let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
-    const currentQuiz = quizData[currentQuestion];
-    questionE1.innerHTML = quizData[currentQuestion];
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
-    currentQuestion++;
 }
+
+function getSelected() {
+    const answerEls = document.querySelectorAll('.answer');
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if (answer) {
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            alert('You finished!');
+        }
+    }
+});
